53d52cc7f6f66b65a86f2a6b85f1629d26c94dba
pennlio <pennlio@gmail.com>
2017-03-16 00:09:57 -0400

    add adapters

---
+++ mj_env/checkerAdapter.py
@@ -0,0 +1,35 @@
+from mj_env.tile import Tile
+import textwrap
+
+
+class CheckerAdapter(object):
+
+    __char_to_letter = {
+        "万":"m",
+        "筒":"p",
+        "条":"s",
+        "字":"z"
+    }
+    __letter_to_char = {
+        "m":"万",
+        "p":"筒",
+        "s":"条",
+        "z":"字"
+    }
+
+    @staticmethod
+    def tile_to_string(tile):
+        return str(tile.number) + CheckerAdapter.__char_to_letter[tile.family]
+
+    @staticmethod
+    def string_to_tile(s):
+        return Tile(CheckerAdapter.__letter_to_char[s[1]], int(s[0]))  # todo: to fix lost id information after the transform
+
+    @staticmethod
+    def transform_to_string(tiles):
+        return [CheckerAdapter.tile_to_string(x) for x in tiles]
+
+    @staticmethod
+    def transform_to_tiles(tile_string):
+        tile_list = textwrap.wrap(tile_string, 2)
+        return [CheckerAdapter.string_to_tile(x) for x in tile_list]
---
+++ mj_env/tests/checkerAdapterTest.py
@@ -0,0 +1,103 @@
+from mj_env.checkerAdapter import CheckerAdapter
+from mj_env.player import HandTile
+from mj_env.tile import Tile
+import unittest
+
+
+class TestCheckerAdapterMethods(unittest.TestCase):
+
+    def test_transform(self):
+        win_tile1 = [
+            Tile("万", 1, 0),
+            Tile("万", 1, 1),
+            Tile("万", 3, 0),
+            Tile("万", 4, 0),
+            Tile("万", 5, 0),
+            Tile("条", 7, 0),
+            Tile("条", 9, 0),
+            Tile("条", 8, 0),
+            Tile("条", 2, 0),
+            Tile("条", 2, 1),
+            Tile("条", 2, 2),
+            Tile("筒", 1, 0),
+            Tile("筒", 1, 2),
+            Tile("筒", 1, 1)
+            ]
+        win_tile2 = [
+            Tile("万", 1, 0),
+            Tile("万", 1, 1),
+            Tile("万", 2, 0),
+            Tile("万", 2, 1),
+            Tile("万", 3, 0),
+            Tile("万", 3, 1),
+            Tile("万", 4, 0),
+            Tile("万", 4, 1),
+            Tile("条", 2, 0),
+            Tile("条", 2, 1),
+            Tile("条", 2, 2),
+            Tile("筒", 1, 0),
+            Tile("筒", 1, 2),
+            Tile("筒", 1, 1)
+            ]
+
+        win_tile3 = [
+            Tile("万", 1, 0),
+            Tile("万", 1, 1),
+            Tile("万", 1, 2),
+            Tile("万", 2, 0),
+            Tile("万", 2, 1),
+            Tile("万", 2, 1),
+            Tile("万", 4, 0),
+            Tile("万", 4, 1),
+            Tile("条", 2, 0),
+            Tile("条", 2, 1),
+            Tile("条", 2, 2),
+            Tile("筒", 1, 0),
+            Tile("筒", 1, 2),
+            Tile("筒", 1, 1)
+            ]
+
+        no_win_tile1 = [
+            Tile("万", 1, 0),
+            Tile("万", 1, 1),
+            Tile("万", 1, 2),
+            Tile("万", 2, 0),
+            Tile("万", 2, 1),
+            Tile("万", 2, 1),
+            Tile("万", 4, 0),
+            Tile("万", 4, 1),
+            Tile("条", 2, 0),
+            Tile("条", 2, 1),
+            Tile("条", 2, 2),
+            Tile("筒", 1, 0),
+            Tile("筒", 1, 2),
+            Tile("筒", 2, 1)
+        ]
+
+        no_win_tile2 = [
+            Tile("万", 1, 0),
+            Tile("万", 1, 1),
+            Tile("万", 1, 2),
+            Tile("万", 2, 0),
+            Tile("万", 2, 0),
+            Tile("万", 5, 0),
+            Tile("万", 5, 1),
+            Tile("条", 2, 1),
+            Tile("条", 2, 2),
+            Tile("条", 4, 0),
+            Tile("条", 4, 1),
+            Tile("筒", 1, 0),
+            Tile("筒", 2, 0),
+            Tile("筒", 3, 0)
+        ]
+
+        self.assertEquals(
+            CheckerAdapter.transform_to_tiles("".join(CheckerAdapter.transform_to_string(win_tile1))), win_tile1)
+        self.assertEquals(
+            CheckerAdapter.transform_to_tiles("".join(CheckerAdapter.transform_to_string(win_tile2))), win_tile2)
+        self.assertEquals(
+            CheckerAdapter.transform_to_tiles("".join(CheckerAdapter.transform_to_string(win_tile3))), win_tile3)
+        self.assertEquals(
+            CheckerAdapter.transform_to_tiles("".join(CheckerAdapter.transform_to_string(no_win_tile1))), no_win_tile1)
+        self.assertEquals(
+            CheckerAdapter.transform_to_tiles("".join(CheckerAdapter.transform_to_string(no_win_tile2))), no_win_tile2)
